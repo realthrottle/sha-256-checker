9936aea180403fa8de92fce1f572c17685d9436a
Throttle <105448954+realthrottle@users.noreply.github.com>
2026-01-24 11:54:15 +0000

    Revert changes to 1.0.0 for 1.0.0 branch

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ import hashlib
 
 print("HashMatch by Throttle.")
 
-print("Tip: Copy and paste the file location and hash.")
-print("Please do not type quotation marks.")
 file = input("Please enter the file path: ")
-print("Please do not type sha256: before the hash.")
-expectedsha256 = input("Please enter the expected SHA-256 hash: ").lower()
-print("If there's no MD5, you can leave this box blank.")
-expectedmd5 = input("Please enter the expected MD5 hash: ").lower()
+expectedsha256 = input("Please enter the expected SHA-256 hash: ")
+expectedmd5 = input("Please enter the expected MD5 hash: ")
 
 with open(file, "rb") as f:
     sha256hash = hashlib.sha256(f.read()).hexdigest()
@@ -22,12 +18,12 @@ print(f"The expected SHA-256 was: {expectedsha256}")
 print(f"The MD5 hash is: {md5hash}")
 print(f"The expected MD5 was: {expectedmd5}")
 
-if sha256hash == expectedsha256.lower():
+if sha256hash == expectedsha256:
     print("The SHA-256 hash matches the expected SHA-256 hash.")
 else:
     print("The SHA-256 hash does not match the expected SHA-256 hash.")
 
-if md5hash == expectedmd5.lower():
+if md5hash == expectedmd5:
     print("The MD5 hash matches the expected MD5 hash.")
 else:
     print("The MD5 hash does not match the expected MD5 hash.")
